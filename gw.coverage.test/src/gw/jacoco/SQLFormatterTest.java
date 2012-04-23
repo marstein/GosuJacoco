7ab16c7d62d8b7f36b09a5641c84ec5650f9ee2b
Martin Stein <mstein@guidewire.com>
2012-04-23 11:42:05 -0700

    Cleaned out Tycho stuff.

--- gw.coverage.test/src/gw/jacoco/SQLFormatterTest.java
+++ gw.coverage.test/src/gw/jacoco/SQLFormatterTest.java
@@ -13,9 +13,12 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
@@ -35,6 +38,8 @@ public class SQLFormatterTest {
 
   private Connection connection;
 
+  static Logger logger = Logger.getLogger("sql formatter test");
+
   @Before
   public void setup() throws Exception {
     driver = new ReportStructureTestDriver();
@@ -44,14 +49,14 @@ public class SQLFormatterTest {
       Class.forName("org.h2.Driver");
       this.connection = DriverManager.getConnection(connectString, "sa", "");
       Statement createTable = connection.createStatement();
-      createTable.executeUpdate("CREATE TABlE  COVERAGE (INSTRUCTION_MISSED integer, INSTRUCTION_COVERED integer, BRANCH_MISSED integer, BRANCH_COVERED integer, LINE_MISSED integer, LINE_COVERED integer, COMPLEXITY_MISSED integer, COMPLEXITY_COVERED integer, METHOD_MISSED integer, METHOD_COVERED integer, branch varchar(100), changelist varchar(30), suite varchar(100), package varchar(100), class varchar(100), suite_run_date timestamp)");
+      createTable.executeUpdate("CREATE TABLE COVERAGE (INSTRUCTION_MISSED integer, INSTRUCTION_COVERED integer, BRANCH_MISSED integer, BRANCH_COVERED integer, LINE_MISSED integer, LINE_COVERED integer, COMPLEXITY_MISSED integer, COMPLEXITY_COVERED integer, METHOD_MISSED integer, METHOD_COVERED integer, branch varchar(100), changelist varchar(30), suite varchar(100), package varchar(100), class varchar(100), suite_run_date timestamp)");
     } catch (ClassNotFoundException e) {
       throw new IllegalStateException("Could not load org.h2.Driver database driver", e);
     } catch (SQLException e) {
       throw new IllegalStateException("Could not get org.h2.Driver database connection to " + connectString, e);
     }
 
-    visitor = formatter.createVisitor(connection, "branch", "changelist", "test suite", new Date());
+    visitor = formatter.createVisitor(connection, "branch", "changelist", "test suite", new SimpleDateFormat().parse("07/10/12 4:5 PM, PDT"));
   }
 
   @After
@@ -63,7 +68,7 @@ public class SQLFormatterTest {
   public void testStructureWithGroup() throws IOException {
     driver.sendGroup(visitor);
     final List<String> lines = getLines();
-    assertEquals("INSTRUCTION_MISSED=10 INSTRUCTION_COVERED=15 BRANCH_MISSED=1 BRANCH_COVERED=2 LINE_MISSED=0 LINE_COVERED=3 COMPLEXITY_MISSED=1 COMPLEXITY_COVERED=2 METHOD_MISSED=0 METHOD_COVERED=1 SUITE=org/jacoco/example PACKAGE= CLASS=org/jacoco/example ",
+    assertEquals("INSTRUCTION_MISSED=10 INSTRUCTION_COVERED=15 BRANCH_MISSED=1 BRANCH_COVERED=2 LINE_MISSED=0 LINE_COVERED=3 COMPLEXITY_MISSED=1 COMPLEXITY_COVERED=2 METHOD_MISSED=0 METHOD_COVERED=1 SUITE=org/jacoco/example PACKAGE= CLASS=org/jacoco/example SUITE_RUN_DATE=null BRANCH= CHANGELIST= ",
             lines.get(0));
   }
 
@@ -71,9 +76,9 @@ public class SQLFormatterTest {
   public void testStructureWithNestedGroups() throws IOException {
     driver.sendNestedGroups(visitor);
     final List<String> lines = getLines();
-    assertEquals("INSTRUCTION_MISSED=10 INSTRUCTION_COVERED=15 BRANCH_MISSED=1 BRANCH_COVERED=2 LINE_MISSED=0 LINE_COVERED=3 COMPLEXITY_MISSED=1 COMPLEXITY_COVERED=2 METHOD_MISSED=0 METHOD_COVERED=1 SUITE=org/jacoco/example PACKAGE= CLASS=org/jacoco/example ",
+    assertEquals("INSTRUCTION_MISSED=10 INSTRUCTION_COVERED=15 BRANCH_MISSED=1 BRANCH_COVERED=2 LINE_MISSED=0 LINE_COVERED=3 COMPLEXITY_MISSED=1 COMPLEXITY_COVERED=2 METHOD_MISSED=0 METHOD_COVERED=1 SUITE=org/jacoco/example PACKAGE= CLASS=org/jacoco/example SUITE_RUN_DATE=null BRANCH= CHANGELIST= ",
             lines.get(0));
-    assertEquals("INSTRUCTION_MISSED=10 INSTRUCTION_COVERED=15 BRANCH_MISSED=1 BRANCH_COVERED=2 LINE_MISSED=0 LINE_COVERED=3 COMPLEXITY_MISSED=1 COMPLEXITY_COVERED=2 METHOD_MISSED=0 METHOD_COVERED=1 SUITE=org/jacoco/example PACKAGE= CLASS=org/jacoco/example ",
+    assertEquals("INSTRUCTION_MISSED=10 INSTRUCTION_COVERED=15 BRANCH_MISSED=1 BRANCH_COVERED=2 LINE_MISSED=0 LINE_COVERED=3 COMPLEXITY_MISSED=1 COMPLEXITY_COVERED=2 METHOD_MISSED=0 METHOD_COVERED=1 SUITE=org/jacoco/example PACKAGE= CLASS=org/jacoco/example SUITE_RUN_DATE=null BRANCH= CHANGELIST= ",
             lines.get(1));
   }
 
@@ -81,17 +86,17 @@ public class SQLFormatterTest {
   public void testStructureWithBundleOnly() throws IOException {
     driver.sendBundle(visitor);
     final List<String> lines = getLines();
-    assertEquals("INSTRUCTION_MISSED=10 INSTRUCTION_COVERED=15 BRANCH_MISSED=1 BRANCH_COVERED=2 LINE_MISSED=0 LINE_COVERED=3 COMPLEXITY_MISSED=1 COMPLEXITY_COVERED=2 METHOD_MISSED=0 METHOD_COVERED=1 SUITE=org/jacoco/example PACKAGE=test suite CLASS=org/jacoco/example ",
+    assertEquals("INSTRUCTION_MISSED=10 INSTRUCTION_COVERED=15 BRANCH_MISSED=1 BRANCH_COVERED=2 LINE_MISSED=0 LINE_COVERED=3 COMPLEXITY_MISSED=1 COMPLEXITY_COVERED=2 METHOD_MISSED=0 METHOD_COVERED=1 SUITE=org/jacoco/example PACKAGE=test suite CLASS=org/jacoco/example SUITE_RUN_DATE=2012-07-10 04:05:00.0 BRANCH=branch CHANGELIST=changelist ",
             lines.get(0));
   }
 
   @Test
   public void testSetEncoding() throws Exception {
     formatter.setOutputEncoding("UTF-16");
-    visitor = formatter.createVisitor(connection, "branch", "changelist", "test suite", new Date());
+    visitor = formatter.createVisitor(connection, "branch", "changelist", "test suite", new SimpleDateFormat().parse("11/11/12 4:5 PM, PDT"));
     driver.sendBundle(visitor);
     final List<String> lines = getLines("UTF-16");
-    assertEquals("INSTRUCTION_MISSED=10 INSTRUCTION_COVERED=15 BRANCH_MISSED=1 BRANCH_COVERED=2 LINE_MISSED=0 LINE_COVERED=3 COMPLEXITY_MISSED=1 COMPLEXITY_COVERED=2 METHOD_MISSED=0 METHOD_COVERED=1 SUITE=org/jacoco/example PACKAGE=test suite CLASS=org/jacoco/example ", lines.get(0));
+    assertEquals("INSTRUCTION_MISSED=10 INSTRUCTION_COVERED=15 BRANCH_MISSED=1 BRANCH_COVERED=2 LINE_MISSED=0 LINE_COVERED=3 COMPLEXITY_MISSED=1 COMPLEXITY_COVERED=2 METHOD_MISSED=0 METHOD_COVERED=1 SUITE=org/jacoco/example PACKAGE=test suite CLASS=org/jacoco/example SUITE_RUN_DATE=2012-11-11 04:05:00.0 BRANCH=branch CHANGELIST=changelist ", lines.get(0));
   }
 
   @Test
@@ -126,11 +131,14 @@ public class SQLFormatterTest {
     final List<String> lines = new ArrayList<String>();
     try {
       Statement select = connection.createStatement();
-      ResultSet result = select.executeQuery("SELECT INSTRUCTION_MISSED, INSTRUCTION_COVERED, BRANCH_MISSED, BRANCH_COVERED, LINE_MISSED, LINE_COVERED, COMPLEXITY_MISSED, COMPLEXITY_COVERED, METHOD_MISSED, METHOD_COVERED, suite, package, class, suite_run_date FROM COVERAGE");
+      ResultSet result = select.executeQuery("SELECT INSTRUCTION_MISSED, INSTRUCTION_COVERED, BRANCH_MISSED, BRANCH_COVERED, LINE_MISSED, LINE_COVERED, COMPLEXITY_MISSED, COMPLEXITY_COVERED, METHOD_MISSED, METHOD_COVERED, suite, package, class, suite_run_date, branch, changelist FROM COVERAGE");
       while (result.next()) {
         StringBuffer line = new StringBuffer();
-        for (int i = 1; i < result.getMetaData().getColumnCount(); i++) {
-          line.append(result.getMetaData().getColumnName(i)).append("=").append(result.getObject(i).toString()).append(" ");
+        for (int i = 1; i <= result.getMetaData().getColumnCount(); i++) {
+//          logger.info("result.getMetaData().getColumnName("+i+")="+result.getMetaData().getColumnName(i));
+          String value = result.getObject(i) == null?"null":result.getObject(i).toString();
+//          logger.info("result.getObject(i)="+value);
+          line.append(result.getMetaData().getColumnName(i)).append("=").append(value).append(" ");
         }
         lines.add(line.toString());
       }
